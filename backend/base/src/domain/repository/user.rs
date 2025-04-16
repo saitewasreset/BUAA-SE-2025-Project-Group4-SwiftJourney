848d5f9f5eabfaad6ec175b999805c8da442ec19
saitewasreset <saiteTimedOut@outlook.com>
2025-04-16 15:11:12 +0800

    feat(UserRepository): 新增根据身份证号查找功能

--- backend/base/src/domain/repository/user.rs
+++ backend/base/src/domain/repository/user.rs
@@ -1,9 +1,14 @@
-use crate::domain::model::user::{Phone, User};
+use crate::domain::model::user::{IdentityCardId, Phone, User};
 use crate::domain::{Repository, RepositoryError};
 
 pub trait UserRepository: Repository<User> {
     fn find_by_phone(
         &self,
         phone: Phone,
+    ) -> impl Future<Output = Result<Option<User>, RepositoryError>> + Send;
+
+    fn find_by_identity_card_id(
+        &self,
+        identity_card_id: IdentityCardId,
     ) -> impl Future<Output = Result<Option<User>, RepositoryError>> + Send;
 }
