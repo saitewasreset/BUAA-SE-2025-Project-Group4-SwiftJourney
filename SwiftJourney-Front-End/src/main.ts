53dc0d818d36f3fa7d230a13a5ccf236d641dc4d
Arekaldi <90160315+Arekaldi@users.noreply.github.com>
2025-04-17 23:55:23 +0800

    build(main): import el-icon

--- SwiftJourney-Front-End/src/main.ts
+++ SwiftJourney-Front-End/src/main.ts
@@ -12,6 +12,12 @@ import router from './router'
 
 const app = createApp(App)
 
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
