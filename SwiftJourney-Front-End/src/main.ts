9b0c26eaa8a31fe5ff5ec78ea313983266ce1476
Arekaldi <90160315+Arekaldi@users.noreply.github.com>
2025-04-17 21:05:07 +0800

    build: import ant-design-vue, supporting more components.

--- SwiftJourney-Front-End/src/main.ts
+++ SwiftJourney-Front-End/src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +15,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.use(Antd);
 
 app.mount('#app')
