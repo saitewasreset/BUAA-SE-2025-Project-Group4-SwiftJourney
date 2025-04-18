e9f2fa2d1f281d4927e7fcc1d81ba3cf7fb55386
saitewasreset <saiteTimedOut@outlook.com>
2025-04-18 21:00:29 +0800

    feat(GeneralError): 对于常见的错误类型，使用`GeneralError`表示

--- backend/base/src/application/mod.rs
+++ backend/base/src/application/mod.rs
@@ -1,7 +1,53 @@
+use dyn_fmt::AsStrFormatExt;
+use shared::{
+    API_BAD_REQUEST_MESSAGE_TEMPLATE, API_FORBIDDEN_MESSAGE_TEMPLATE,
+    API_INTERNAL_SERVER_ERROR_MESSAGE,
+};
+use thiserror::Error;
+
 pub mod commands;
 pub mod service;
 
 pub trait ApplicationError: std::error::Error + 'static {
     fn error_code(&self) -> u32;
     fn error_message(&self) -> String;
+}
+
+#[derive(Error, Debug)]
+pub enum GeneralError {
+    /// 会话ID无效
+    #[error("invalid session id")]
+    InvalidSessionId,
+    /// 请求参数无效
+    #[error("{0}")]
+    BadRequest(String),
+    /// 服务器内部错误
+    #[error("an internal server error occurred")]
+    InternalServerError,
+}
+
+impl From<GeneralError> for Box<dyn ApplicationError> {
+    fn from(err: GeneralError) -> Self {
+        Box::new(err)
+    }
+}
+
+impl ApplicationError for GeneralError {
+    fn error_code(&self) -> u32 {
+        match self {
+            GeneralError::BadRequest(_) => 400,
+            GeneralError::InvalidSessionId => 403,
+            GeneralError::InternalServerError => 500,
+        }
+    }
+
+    fn error_message(&self) -> String {
+        match self {
+            GeneralError::BadRequest(info) => API_BAD_REQUEST_MESSAGE_TEMPLATE.format(&[info]),
+            GeneralError::InvalidSessionId => {
+                API_FORBIDDEN_MESSAGE_TEMPLATE.format(&["invalid session id"])
+            }
+            GeneralError::InternalServerError => API_INTERNAL_SERVER_ERROR_MESSAGE.to_owned(),
+        }
+    }
 }
