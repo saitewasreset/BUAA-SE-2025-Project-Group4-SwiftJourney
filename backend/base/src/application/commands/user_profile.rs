2d5cf8a97f350072bc6acbff005f59d9c48843ff
saitewasreset <saiteTimedOut@outlook.com>
2025-04-18 17:34:42 +0800

    feat(UserProfileService): 新增`SetUserProfileDTO`结构; 为DTO实现常用特征

--- backend/base/src/application/commands/user_profile.rs
+++ backend/base/src/application/commands/user_profile.rs
@@ -1,3 +1,5 @@
+use crate::application::service::user_profile::SetUserProfileDTO;
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct UserProfileQuery {
     pub session_id: String,
@@ -10,4 +12,16 @@ pub struct SetUserProfileCommand {
     pub gender: Option<String>,
     pub age: Option<u16>,
     pub email: String,
+}
+
+impl SetUserProfileCommand {
+    pub fn from_session_id_and_dto(session_id: String, dto: SetUserProfileDTO) -> Self {
+        SetUserProfileCommand {
+            session_id,
+            username: dto.username,
+            gender: dto.gender,
+            age: dto.age,
+            email: dto.email,
+        }
+    }
 }
