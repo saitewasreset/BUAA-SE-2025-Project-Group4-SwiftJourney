6d27a5a84b391ff84549cccfd6d3f5e43045e964
saitewasreset <saiteTimedOut@outlook.com>
2025-04-23 19:13:06 +0800

    refactor(City): 使用`CityName`、`ProvinceName` DP

--- backend/base/src/domain/model/city.rs
+++ backend/base/src/domain/model/city.rs
@@ -1,13 +1,47 @@
 use crate::domain::{Entity, Identifiable, Identifier};
 use id_macro::define_id_type;
+use std::ops::Deref;
 
 define_id_type!(City);
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct CityName(String);
+
+impl From<String> for CityName {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl Deref for CityName {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ProvinceName(String);
+
+impl Deref for ProvinceName {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<String> for ProvinceName {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct City {
     city_id: Option<CityId>,
-    name: String,
-    province: String,
+    name: CityName,
+    province: ProvinceName,
 }
 
 impl Identifiable for City {
@@ -24,7 +58,7 @@ impl Identifiable for City {
 impl Entity for City {}
 
 impl City {
-    pub fn new(name: String, province: String) -> Self {
+    pub fn new(name: CityName, province: ProvinceName) -> Self {
         City {
             city_id: None,
             name,
@@ -32,11 +66,11 @@ impl City {
         }
     }
 
-    pub fn name(&self) -> &str {
+    pub fn name(&self) -> &CityName {
         &self.name
     }
 
-    pub fn province(&self) -> &str {
+    pub fn province(&self) -> &ProvinceName {
         &self.province
     }
 }
