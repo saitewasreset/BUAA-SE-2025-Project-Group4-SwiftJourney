0f1996f4842c78979cf25da5b80bbad22e558c0e
Arekaldi <90160315+Arekaldi@users.noreply.github.com>
2025-04-28 18:00:09 +0800

    feat(user): import debugUserStore to disguise logined user.

--- SwiftJourney-Front-End/src/stores/user.ts
+++ SwiftJourney-Front-End/src/stores/user.ts
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { NormalConstants } from '@/constant/NormalConstant';
 
 export const useUserStore = defineStore('user', {
     state: () => ({
@@ -8,7 +9,12 @@ export const useUserStore = defineStore('user', {
         identityCardId: '',
         preferredSeatLocation: 'A' as 'A' | 'B' | 'C' | 'D' | 'F',
         token: '',
-        default: false,
+        isLogin: false,
+        gender: "male" as "male" | "female",
+        age: 0,
+        phone: '',
+        email: '',
+        havePaymentPasswordSet: false,
     }),
     getters: {
 
@@ -21,6 +27,48 @@ export const useUserStore = defineStore('user', {
             this.personalId = personalId;
             this.name = name;
             this.identityCardId = identityCardId;
+        },
+        setUserLoginStatus(isLogin: boolean) {
+            this.isLogin = isLogin;
+        },
+        logout() {
+
         }
     }
-})
+});
+
+export const useDebugUserStore = defineStore('debugUser', {
+    state: () => ({
+        personalId: '123456789012345678',
+        name: 'John Doe',
+        identityCardId: '123456789012345678',
+        preferredSeatLocation: 'A' as 'A' | 'B' | 'C' | 'D' | 'F',
+        token: '',
+        isLogin: true,
+        gender: "male",
+        age: 20,
+        phone: '15338297650',
+        email: 'john.doe@example.com',
+        havePaymentPasswordSet: false,
+        remainingMoney: NormalConstants.RMB_SIGNAL + '30000',
+    }),
+    getters: {
+
+    },
+    actions: {
+        setPreferredSeatLocation(location: 'A' | 'B' | 'C' | 'D' | 'F') {
+            this.preferredSeatLocation = location;
+        },
+        setUserDetails(personalId: string, name: string, identityCardId: string) {
+            this.personalId = personalId;
+            this.name = name;
+            this.identityCardId = identityCardId;
+        },
+        setUserLoginStatus(isLogin: boolean) {
+            this.isLogin = isLogin;
+        },
+        logout() {
+            
+        }
+    }
+});
