1119b1be75e96d525923a2f0950371eb194eb68c
saitewasreset <saiteTimedOut@outlook.com>
2025-05-01 11:36:51 +0800

    feat: 实现`RouteRepository`

--- backend/base/src/domain/model/route.rs
+++ backend/base/src/domain/model/route.rs
@@ -6,6 +6,7 @@ define_id_type!(Stop);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Stop {
     stop_id: Option<StopId>,
+    route_id: Option<RouteId>,
     station_id: StationId,
     arrival_time: u32,
     departure_time: u32,
@@ -26,6 +27,42 @@ impl Identifiable for Stop {
 
 impl Entity for Stop {}
 
+impl Stop {
+    pub fn new(
+        id: Option<StopId>,
+        route_id: Option<RouteId>,
+        station_id: StationId,
+        arrival_time: u32,
+        departure_time: u32,
+        order: u32,
+    ) -> Self {
+        Stop {
+            stop_id: id,
+            route_id,
+            station_id,
+            arrival_time,
+            departure_time,
+            order,
+        }
+    }
+
+    pub fn station_id(&self) -> StationId {
+        self.station_id
+    }
+
+    pub fn arrival_time(&self) -> u32 {
+        self.arrival_time
+    }
+
+    pub fn departure_time(&self) -> u32 {
+        self.departure_time
+    }
+
+    pub fn order(&self) -> u32 {
+        self.order
+    }
+}
+
 define_id_type!(Route);
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Route {
@@ -41,10 +78,50 @@ impl Identifiable for Route {
     }
 
     fn set_id(&mut self, id: Self::ID) {
-        self.route_id = Some(id)
+        self.route_id = Some(id);
+
+        for stop in &mut self.stops {
+            stop.route_id = Some(id);
+        }
     }
 }
 
 impl Entity for Route {}
 
 impl Aggregate for Route {}
+
+impl Route {
+    pub fn new(id: Option<RouteId>) -> Self {
+        Route {
+            route_id: id,
+            stops: Vec::new(),
+        }
+    }
+
+    pub fn stops(&self) -> &[Stop] {
+        &self.stops
+    }
+
+    pub fn into_stops(self) -> Vec<Stop> {
+        self.stops
+    }
+
+    pub fn add_stop(
+        &mut self,
+        stop_id: Option<StopId>,
+        station_id: StationId,
+        arrival_time: u32,
+        departure_time: u32,
+        order: u32,
+    ) {
+        let stop = Stop::new(
+            stop_id,
+            self.route_id,
+            station_id,
+            arrival_time,
+            departure_time,
+            order,
+        );
+        self.stops.push(stop);
+    }
+}
--- backend/base/src/domain/repository/route.rs
+++ backend/base/src/domain/repository/route.rs
@@ -1,6 +1,12 @@
-use crate::domain::Repository;
-use crate::domain::model::route::Route;
+use crate::domain::model::route::{Route, RouteId};
+use crate::domain::{Repository, RepositoryError};
 use async_trait::async_trait;
+use shared::data::RouteStationInfo;
 
 #[async_trait]
-pub trait RouteRepository: Repository<Route> {}
+pub trait RouteRepository: Repository<Route> {
+    async fn load(&self) -> Result<Vec<Route>, RepositoryError>;
+
+    async fn save_raw(&self, raw_routes: Vec<RouteStationInfo>)
+    -> Result<RouteId, RepositoryError>;
+}
