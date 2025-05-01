f99fa4f3073256d389b6daa494d733c09e41348c
saitewasreset <saiteTimedOut@outlook.com>
2025-05-01 13:47:41 +0800

    feat(api): 新增火车数据加载相关API端点

--- backend/apps/api/src/main.rs
+++ backend/apps/api/src/main.rs
@@ -30,6 +30,7 @@
  */
 use actix_web::{App, HttpServer, web};
 use api::{AppConfig, MAX_BODY_LENGTH};
+use base::application::service::train_data::TrainDataService;
 use base::application::service::user_manager::UserManagerService;
 use base::application::service::user_profile::UserProfileService;
 use base::domain::model::session_config::SessionConfig;
@@ -37,9 +38,14 @@ use base::domain::repository::session::SessionRepositoryConfig;
 use base::domain::repository::user::UserRepository;
 use base::domain::service::session::SessionManagerService;
 use base::domain::service::user::UserService;
+use base::infrastructure::application::service::train_data::TrainDataServiceImpl;
 use base::infrastructure::application::service::user_manager::UserManagerServiceImpl;
 use base::infrastructure::application::service::user_profile::UserProfileServiceImpl;
+use base::infrastructure::repository::city::CityRepositoryImpl;
+use base::infrastructure::repository::route::RouteRepositoryImpl;
 use base::infrastructure::repository::session::SessionRepositoryImpl;
+use base::infrastructure::repository::station::StationRepositoryImpl;
+use base::infrastructure::repository::train::TrainRepositoryImpl;
 use base::infrastructure::repository::user::UserRepositoryImpl;
 use base::infrastructure::service::password::Argon2PasswordServiceImpl;
 use base::infrastructure::service::session::SessionManagerServiceImpl;
@@ -81,6 +87,11 @@ async fn main() -> std::io::Result<()> {
         ));
 
     let user_repository_impl = Arc::new(UserRepositoryImpl::new(conn.clone()));
+    let city_repository_impl = Arc::new(CityRepositoryImpl::new(conn.clone()));
+    let station_repository_impl = Arc::new(StationRepositoryImpl::new(conn.clone()));
+    let train_repository_impl = Arc::new(TrainRepositoryImpl::new(conn.clone()));
+    let route_repository_impl = Arc::new(RouteRepositoryImpl::new(conn.clone()));
+
     let user_service_impl = Arc::new(UserServiceImpl::<_, Argon2PasswordServiceImpl>::new(
         Arc::clone(&user_repository_impl),
     ));
@@ -94,6 +105,14 @@ async fn main() -> std::io::Result<()> {
     let user_profile_service_impl = Arc::new(UserProfileServiceImpl::new(
         Arc::clone(&session_manager_service_impl),
         Arc::clone(&user_repository_impl),
+    ));
+
+    let train_data_service_impl = Arc::new(TrainDataServiceImpl::new(
+        debug_mode,
+        Arc::clone(&city_repository_impl),
+        Arc::clone(&station_repository_impl),
+        Arc::clone(&train_repository_impl),
+        Arc::clone(&route_repository_impl),
     ));
 
     let user_repository: web::Data<dyn UserRepository> =
@@ -111,6 +130,9 @@ async fn main() -> std::io::Result<()> {
     let user_profile_service: web::Data<dyn UserProfileService> =
         web::Data::from(user_profile_service_impl as Arc<dyn UserProfileService>);
 
+    let train_data_service: web::Data<dyn TrainDataService> =
+        web::Data::from(train_data_service_impl as Arc<dyn TrainDataService>);
+
     let app_config = AppConfig { debug: debug_mode };
 
     // Step 2: Create instance of your application service,
@@ -126,6 +148,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(user_service.clone())
             .app_data(user_manager_service.clone())
             .app_data(user_profile_service.clone())
+            .app_data(train_data_service.clone())
             // Step 3: Register your application service using `.app_data` function
             // Exercise 1.2.1D - 6: Your code here. (2 / 2)
             // Thinking 1.2.1D - 8: `App::new().app_data(...).app_data(...)`是什么设计模式的体现？
@@ -135,7 +158,8 @@ async fn main() -> std::io::Result<()> {
             .service(
                 web::scope("/api")
                     .service(web::scope("/user").configure(api::user::scoped_config))
-                    .service(web::scope("/general").configure(api::general::scoped_config)),
+                    .service(web::scope("/general").configure(api::general::scoped_config))
+                    .service(web::scope("/data").configure(api::data::scoped_config)),
             )
         // Step 6: Register your endpoint using `.service()` function
         // Exercise 1.2.1D - 7: Your code here. (5 / 5)
