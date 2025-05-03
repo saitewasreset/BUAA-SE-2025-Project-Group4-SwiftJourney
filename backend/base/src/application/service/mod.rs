6a1c85e194b093e5823267db2b8e5fe02016c967
saitewasreset <saiteTimedOut@outlook.com>
2025-05-03 11:13:44 +0800

    feat: close #69034000 实现mode、city、city_stations全局状态API

--- backend/base/src/application/service/mod.rs
+++ backend/base/src/application/service/mod.rs
@@ -1,6 +1,7 @@
 pub mod personal_info;
 pub mod user_profile;
 
+pub mod geo;
 pub mod train_data;
 pub mod train_query;
 pub mod user_manager;
--- backend/base/src/infrastructure/application/service/mod.rs
+++ backend/base/src/infrastructure/application/service/mod.rs
@@ -1,3 +1,4 @@
+pub mod geo;
 pub mod train_data;
 pub mod train_query;
 pub mod user_manager;
