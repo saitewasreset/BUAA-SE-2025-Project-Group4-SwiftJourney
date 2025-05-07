b4ae9c17958b3571f972db4ec705ecbe09da7975
saitewasreset <saiteTimedOut@outlook.com>
2025-05-07 21:00:54 +0800

    feat: 实现`TransactionApplicationService`

---
+++ backend/base/src/application/commands/transaction.rs
@@ -0,0 +1,43 @@
+use uuid::Uuid;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RechargeCommand {
+    pub session_id: String,
+    pub amount: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct BalanceQuery {
+    pub session_id: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TransactionQuery {
+    pub session_id: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct SetPaymentPasswordCommand {
+    pub session_id: String,
+    pub user_password: String,
+    pub payment_password: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct PayTransactionCommand {
+    pub session_id: String,
+    pub transaction_id: Uuid,
+    pub user_password: Option<String>,
+    pub payment_password: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct OrderListQuery {
+    pub session_id: String,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct GenerateDebugTransactionCommand {
+    pub session_id: String,
+    pub amount: f64,
+}
