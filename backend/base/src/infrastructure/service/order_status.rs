2750b89e97730000083b71608dd17b427921b53b
saitewasreset <saiteTimedOut@outlook.com>
2025-05-07 21:19:16 +0800

    feat(OrderStatusMangerService): TODO

---
+++ backend/base/src/infrastructure/service/order_status.rs
@@ -0,0 +1,29 @@
+use crate::domain::model::order::{Order, OrderStatus};
+use crate::domain::service::order_status::{
+    OrderStatusManagerService, OrderStatusManagerServiceError,
+};
+use async_trait::async_trait;
+use uuid::Uuid;
+
+pub struct OrderStatusManagerServiceImpl {}
+
+impl OrderStatusManagerServiceImpl {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[async_trait]
+impl OrderStatusManagerService for OrderStatusManagerServiceImpl {
+    async fn attach(&self, order: &dyn Order) -> Result<(), OrderStatusManagerServiceError> {
+        todo!()
+    }
+
+    async fn detach(&self, order: &dyn Order) -> Result<(), OrderStatusManagerServiceError> {
+        todo!()
+    }
+
+    async fn notify_status_change(&self, order_uuid: Uuid, new_status: OrderStatus) {
+        todo!()
+    }
+}
