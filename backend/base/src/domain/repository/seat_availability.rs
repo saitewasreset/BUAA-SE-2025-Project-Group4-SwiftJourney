194b8f81700c8e2ab11bf094204103b146a2b9d1
saitewasreset <saiteTimedOut@outlook.com>
2025-05-13 16:09:12 +0800

    feat(SeatAvailabilityRepository): 新增`get_train_schedule_occupied_seat`方法

--- backend/base/src/domain/repository/seat_availability.rs
+++ backend/base/src/domain/repository/seat_availability.rs
@@ -1,6 +1,10 @@
 use crate::domain::model::train_schedule::{SeatAvailability, TrainScheduleId};
 use crate::domain::{Repository, RepositoryError};
 use async_trait::async_trait;
+use std::collections::HashMap;
+
+// seat_type_id -> (begin_station_id, end_station_id) -> [seat_id]
+pub type OccupiedSeatInfoMap = HashMap<i32, HashMap<(i32, i32), Vec<i64>>>;
 
 #[async_trait]
 pub trait SeatAvailabilityRepository: Repository<SeatAvailability> {
@@ -8,4 +12,9 @@ pub trait SeatAvailabilityRepository: Repository<SeatAvailability> {
         &self,
         train_schedule_id: TrainScheduleId,
     ) -> Result<Vec<crate::models::seat_availability::Model>, RepositoryError>;
+
+    async fn get_train_schedule_occupied_seat(
+        &self,
+        train_schedule_id: TrainScheduleId,
+    ) -> Result<OccupiedSeatInfoMap, RepositoryError>;
 }
