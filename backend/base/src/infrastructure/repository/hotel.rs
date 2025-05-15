a3e0c3fec82465c4a448b6c9fcdd7aa08ed8f58a
saitewasreset <saiteTimedOut@outlook.com>
2025-05-15 12:25:23 +0800

    refactor(HotelRoom): 明确其应当为HotelRoomType

--- backend/base/src/infrastructure/repository/hotel.rs
+++ backend/base/src/infrastructure/repository/hotel.rs
@@ -1,5 +1,5 @@
 use crate::domain::DbId;
-use crate::domain::model::hotel::{HotelId, HotelRoomId};
+use crate::domain::model::hotel::{HotelId, HotelRoomTypeId};
 
 impl_db_id_from_u64!(HotelId, i32, "hotel id");
-impl_db_id_from_u64!(HotelRoomId, i32, "hotel room id");
+impl_db_id_from_u64!(HotelRoomTypeId, i32, "hotel room type id");
