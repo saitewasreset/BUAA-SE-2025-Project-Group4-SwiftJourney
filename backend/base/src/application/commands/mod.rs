331ae939c7d99354b773a7f6b320f4e40359a57b
saitewasreset <saiteTimedOut@outlook.com>
2025-05-21 15:52:28 +0800

    feat: close #69034980 实现撰写评价功能

--- backend/base/src/application/commands/mod.rs
+++ backend/base/src/application/commands/mod.rs
@@ -1,7 +1,8 @@
+pub mod hotel;
 pub mod hotel_data;
 pub mod personal_info;
 pub mod train_data;
+pub mod train_query;
 pub mod transaction;
 pub mod user_manager;
 pub mod user_profile;
-pub mod train_query;
