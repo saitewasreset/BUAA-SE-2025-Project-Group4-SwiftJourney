f954c2cb6bccb1c04c76449bcece5f413329125a
saitewasreset <saiteTimedOut@outlook.com>
2025-05-22 17:25:16 +0800

    feat: 新增save_raw_takeaway、save_raw_dish方法

--- backend/base/src/domain/repository/takeaway.rs
+++ backend/base/src/domain/repository/takeaway.rs
@@ -1,8 +1,14 @@
 use crate::domain::model::route::{RouteId, Stop};
 use crate::domain::model::takeaway::TakeawayShop;
+use crate::domain::repository::station::StationRepository;
+use crate::domain::repository::train::TrainRepository;
+use crate::domain::service::object_storage::ObjectStorageService;
 use crate::domain::{Repository, RepositoryError};
 use async_trait::async_trait;
+use shared::data::{DishData, TakeawayData};
 use std::collections::HashMap;
+use std::path::Path;
+use std::sync::Arc;
 
 #[async_trait]
 pub trait TakeawayShopRepository: Repository<TakeawayShop> {
@@ -10,4 +16,14 @@ pub trait TakeawayShopRepository: Repository<TakeawayShop> {
         &self,
         route_id: RouteId,
     ) -> Result<HashMap<Stop, Vec<TakeawayShop>>, RepositoryError>;
+
+    async fn save_many_atomic(&self, entities: Vec<TakeawayShop>) -> Result<(), RepositoryError>;
+
+    async fn save_raw_takeaway<S: StationRepository, OS: ObjectStorageService>(
+        &self,
+        data: TakeawayData,
+        data_path: &Path,
+        station_repository: Arc<S>,
+        object_storage_service: Arc<OS>,
+    ) -> Result<(), RepositoryError>;
 }
