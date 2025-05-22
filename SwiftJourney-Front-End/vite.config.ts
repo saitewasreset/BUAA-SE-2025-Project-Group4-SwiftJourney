efb73395acbb89ecc8dedaec33262004d485a099
Arekaldi <90160315+Arekaldi@users.noreply.github.com>
2025-05-22 14:02:34 +0800

    docs(vite.config): add annotations to sign as dev environment

--- SwiftJourney-Front-End/vite.config.ts
+++ SwiftJourney-Front-End/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // --------- ONLY DEV ------------
   server: {
     proxy: {
       '/api': {
