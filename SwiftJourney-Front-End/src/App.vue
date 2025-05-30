dc48a60fb8aa36d57128c841534a79a9dae45e7c
Lancezer <176793757+Lancezer@users.noreply.github.com>
2025-05-30 11:05:19 +0800

    fix(app): modify views

--- SwiftJourney-Front-End/src/App.vue
+++ SwiftJourney-Front-End/src/App.vue
@@ -1,35 +1,56 @@
 <template>
-  <div style="width: 100%; height: 100%; padding: 0%; margin: 0%;">
-    <TitleBar v-if="shouldTitleBarDisplay" />
-    <RouterView />
+  <div class="root">
+    <TitleBar v-if="shouldTitleBarDisplay" class="title-bar" />
+    <div :class="{ 'router-view-container': true, 'container-margin': shouldTitleBarDisplay }">
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView, useRoute } from 'vue-router'
-import TitleBar from './components/TitleBar/TitleBar.vue';
-import { computed } from 'vue';
+import TitleBar from './components/TitleBar/TitleBar.vue'
+import { computed } from 'vue'
 
-const route = useRoute();
+const route = useRoute()
 
-const undisplayTitleBar : string[] = ["login", "register"];
+const undisplayTitleBar: string[] = ['login', 'register']
 
 const shouldTitleBarDisplay = computed(() => {
   if (undisplayTitleBar.includes(route.name as string)) {
-    return false;
+    return false
   }
-  return true;
-});
+  return true
+})
 </script>
 
 <style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.title-bar {
+  flex-shrink: 0; /* 防止标题栏被压缩 */
+}
+
+.router-view-container {
+  padding: 20px;
+}
+
+.container-margin {
+  margin-top: calc(var(--el-menu-horizontal-height));
+}
+
 #app {
   height: 100%;
   width: 100%;
   padding: 0%;
   margin: 0%;
 }
-header {
+
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -37,31 +58,6 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -80,14 +76,5 @@ nav a:first-of-type {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+} */
 </style>
