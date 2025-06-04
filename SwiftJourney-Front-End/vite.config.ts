839a9f7523e47c1482f866434fbcd442181549fb
Ma-HR <178689431+Ma-HR@users.noreply.github.com>
2025-06-04 20:56:44 +0800

    fix(hotel-search): 将全部获取后加载改为获取一些加载一些

--- SwiftJourney-Front-End/vite.config.ts
+++ SwiftJourney-Front-End/vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:8080',
         changeOrigin: true,
+      },
+      '/resource': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
       }
     }
   }
