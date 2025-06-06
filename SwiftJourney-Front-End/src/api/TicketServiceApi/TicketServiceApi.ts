7010d6944c551f0e8d394c39b8e8546a075f104b
Lancezer <176793757+Lancezer@users.noreply.github.com>
2025-06-07 03:10:33 +0800

    feat(api): add ticket service api wrapper

---
+++ SwiftJourney-Front-End/src/api/TicketServiceApi/TicketServiceApi.ts
@@ -0,0 +1,16 @@
+import { getRequest, postRequest } from '../axios'
+import type {
+  scheduleRequest,
+} from '@/interface/ticketServiceInterface'
+
+export const TicketServiceApi = {
+  // 查询直达车次
+  queryDirectSchedule: (params: scheduleRequest) => {
+    return postRequest('/api/train/schedule/query_direct', params)
+  },
+
+  // 查询中转车次
+  queryIndirectSchedule: (params: scheduleRequest) => {
+    return postRequest('/api/train/schedule/query_indirect', params)
+  },
+}
