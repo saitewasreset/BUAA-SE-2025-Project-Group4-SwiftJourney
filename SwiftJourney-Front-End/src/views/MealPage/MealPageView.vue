4fce58909cef9e44248cd1a37d39d8cb44f4da52
Ma-HR <178689431+Ma-HR@users.noreply.github.com>
2025-06-07 17:39:53 +0800

    feat(meal-query): 完善查询框

---
+++ SwiftJourney-Front-End/src/views/MealPage/MealPageView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="display: flex; justify-content: center; height: 100%;">
+        <MealSearch style="" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import MealSearch from '@/components/MealSearch/MealSearch.vue';
+</script>
+
+<style scoped>
+
+</style>
