446cb238f91a1be2d0698b284d6363b56f78b18d
saitewasreset <saiteTimedOut@outlook.com>
2025-08-27 16:23:23 +0800

    chore: same as before

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -10,7 +10,9 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
-        provider: 'v8' // or 'istanbul'
+        provider: 'v8', // or 'istanbul'
+        enabled: true,
+        include: ['src/**.{ts,tsx,vue}']
       },
     },
   }),
