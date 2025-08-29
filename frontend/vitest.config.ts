0692abeb5e26576d0906584f7ff15013dce2f5a7
Arekaldi <90160315+Arekaldi@users.noreply.github.com>
2025-08-29 09:41:30 +0800

    chore: modify vitest config to moke virtual backend

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -9,6 +9,7 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: "test/setup.ts",
       coverage: {
         provider: 'v8', // or 'istanbul'
         enabled: true,
