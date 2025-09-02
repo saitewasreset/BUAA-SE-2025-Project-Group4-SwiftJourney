373337e07dceb8aa9f9ee564dc48f374317b3b81
saitewasreset <saiteTimedOut@outlook.com>
2025-09-02 20:29:17 +0800

    feat(Train Microservice): 添加有关`TrainInternalService`的相关结构

--- backend/shared/src/internal/train/dto.rs
+++ backend/shared/src/internal/train/dto.rs
@@ -1,19 +1,11 @@
 use chrono::NaiveDate;
-use rust_decimal::Decimal;
+use sea_orm::prelude::Decimal;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct SeatTypeDTO {
-    pub seat_type_id: Option<u64>,
-    pub type_name: String,
-    pub capacity: u32,
-    pub price: Decimal,
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TrainDTO {
-    pub id: Option<u64>,
+    pub id: u64,
     pub number: String,
     pub train_type: String,
     pub seats: HashMap<String, SeatTypeDTO>,
@@ -21,20 +13,26 @@ pub struct TrainDTO {
     pub default_origin_departure_time: i32,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct StationRangeDTO {
-    pub from_station_id: u64,
-    pub to_station_id: u64,
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct SeatTypeDTO {
+    pub seat_type_id: u64,
+    pub type_name: String,
+    pub capacity: u32,
+    pub price: Decimal,
 }
-
-pub type SeatAvailabilityMapDTO = HashMap<StationRangeDTO, HashMap<String, u64>>;
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct TrainScheduleDTO {
-    pub id: Option<u64>,
+    pub id: u64,
     pub train_id: u64,
     pub date: NaiveDate,
     pub origin_departure_time: i32,
     pub route_id: u64,
-    pub seat_availability_map: SeatAvailabilityMapDTO,
+    pub seat_availability_map: HashMap<StationRangeDTO, HashMap<SeatTypeDTO, u64>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct StationRangeDTO {
+    pub from_station_id: u64,
+    pub to_station_id: u64,
 }
