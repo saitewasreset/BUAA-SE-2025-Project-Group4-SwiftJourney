a0e5cf9710bf33ad99623a04d641c8a1e5b2e302
saitewasreset <saiteTimedOut@outlook.com>
2025-09-06 20:26:45 +0800

    chore: 加载文件脚本将主机名放置到变量中

--- data/load.sh
+++ data/load.sh
@@ -1,8 +1,10 @@
-curl -X POST -H "Content-Type: application/json" -d @city.json http://127.0.0.1:8080/api/data/city
-curl -X POST -H "Content-Type: application/json" -d @station.json http://127.0.0.1:8080/api/data/station
-curl -X POST -H "Content-Type: application/json" -d @train_type.json http://127.0.0.1:8080/api/data/train_type
-curl -X POST -H "Content-Type: application/json" -d @train_number.json http://127.0.0.1:8080/api/data/train_number
-curl -X POST -H "Content-Type: application/json" -d @hotels.json http://127.0.0.1:8080/api/data/hotel
+API_HOST="127.0.0.1:8080"
+
+curl -X POST -H "Content-Type: application/json" -d @city.json http://${API_HOST}/api/data/city
+curl -X POST -H "Content-Type: application/json" -d @station.json http:/${API_HOST}/api/data/station
+curl -X POST -H "Content-Type: application/json" -d @train_type.json http://${API_HOST}/api/data/train_type
+curl -X POST -H "Content-Type: application/json" -d @train_number.json http://${API_HOST}/api/data/train_number
+curl -X POST -H "Content-Type: application/json" -d @hotels.json http://${API_HOST}/api/data/hotel
 7z x dish_takeaway.7z
-curl -X POST -H "Content-Type: application/json" -d @dish_takeaway.json http://127.0.0.1:8080/api/data/dish_takeaway
+curl -X POST -H "Content-Type: application/json" -d @dish_takeaway.json http://${API_HOST}/api/data/dish_takeaway
 rm dish_takeaway.json
